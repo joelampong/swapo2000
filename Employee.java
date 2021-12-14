3f9b66f08bf2747cd4b1ef7e1c590cd903cd8008
Joel Ampong <joelampong@yahoo.com>
2021-12-14 19:22:31 +0100

    updated

--- Employee.java
+++ Employee.java
@@ -1,4 +1,9 @@
 Hello the file contains java code
 That is the first java Code to be displayed
+<<<<<<< HEAD
 This file is very important to the devops team
+=======
+Java is the best codeµ
+code to code to code
+>>>>>>> development
 
